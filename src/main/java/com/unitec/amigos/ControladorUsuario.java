76fb14adbc889123bdbbb93fed4856a3e5caa914
Abraham Perez <60354088+atlzero@users.noreply.github.com>
2021-10-15 19:05:22 -0500

    Update ControladorUsuario.java

--- src/main/java/com/unitec/amigos/ControladorUsuario.java
+++ src/main/java/com/unitec/amigos/ControladorUsuario.java
@@ -1,6 +1,6 @@
 package com.unitec.amigos;
 
-
+//Comentario de prueba
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
